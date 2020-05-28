1cb5f615c7d2f3f98a253623dc4e9c4cf5753b4e
liuzhengyang <liuzhengyang75@gmail.com>
2020-05-28 14:31:53 +0800

    rollback hotreload bash version to 1.0.8

--- bin/boot.sh
+++ bin/boot.sh
@@ -2,5 +2,5 @@
 
 echo 'Install with arguments '$@
 
-wget https://maven.aliyun.com/repository/public/com/github/liuzhengyang/hotreload-boot/1.0.9/hotreload-boot-1.0.9-jar-with-dependencies.jar
-java -jar hotreload-boot-1.0.9-jar-with-dependencies.jar $@
+wget https://maven.aliyun.com/repository/public/com/github/liuzhengyang/hotreload-boot/1.0.8/hotreload-boot-1.0.8-jar-with-dependencies.jar
+java -jar hotreload-boot-1.0.8-jar-with-dependencies.jar $@
